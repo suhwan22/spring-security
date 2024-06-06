491a5f55d7fad1eb664640bb158b0bead23d52cb
suhwan <tnghkswkd123@naver.com>
2024-06-06 21:30:00 +0900

    feat: login 한 session 정보를 통해 username 과 role 을 mainP 에 출력

--- src/main/java/test/testsecurity/controller/MainController.java
+++ src/main/java/test/testsecurity/controller/MainController.java
@@ -1,14 +1,32 @@
 package test.testsecurity.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller
 public class MainController {
 
 	@GetMapping("/")
-	public String mainP() {
+	public String mainP(Model model) {
+
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+		Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+		Iterator<? extends  GrantedAuthority> iter = authorities.iterator();
+		GrantedAuthority auth = iter.next();
+
+		String id = authentication.getName();
+		String role = auth.getAuthority();
+
+		model.addAttribute("id", id);
+		model.addAttribute("role", role);
 		return "main";
 	}
-
 }
